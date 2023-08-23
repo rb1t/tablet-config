fc1df3cb9cf16ba2c3d84b1e4527d6c75f94a232
rb1t <rb1t.code@gmail.com>
2023-08-23 19:10:32 -0300

    oops, forgot to add the actual script lol

---
+++ tablet_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+sleep 10
+xsetwacom set "Tablet Monitor Pen stylus" Area 0 0 69000 39000
+xsetwacom set "Tablet Monitor Pen stylus" MapToOutput HEAD-1
+xsetwacom set "Tablet Monitor Pad pad" Button "2" "key +ctrl +z"
+xsetwacom set "Tablet Monitor Pad pad" Button "1" "key +e"
+xsetwacom set "Tablet Monitor Pad pad" Button "8" "key +ctrl +shift +z"
+xsetwacom set "Tablet Monitor Pad pad" Button "9" "key +alt"
+xsetwacom set "Tablet Monitor Pad pad" Button "3" "key +T"
+xsetwacom set "Tablet Monitor Pad pad" Button "10" "key +ctrl"
+xsetwacom set "Tablet Monitor Pad pad" Button "11" "key +shift"
+xsetwacom set "Tablet Monitor Touch Strip pad" Button "1" "key +z"
+xsetwacom set "Tablet Monitor Pad pad" Button "12" "key +plus"
+xsetwacom set "Tablet Monitor Pad pad" Button "13" "key +minus"
+xsetwacom set "Tablet Monitor Pad pad" Button "15" "key +0"
+xsetwacom set "Tablet Monitor Pad pad" Button "14" "key +space"
+xsetwacom set "Tablet Monitor Pad pad" Button "17" "key +0"
+
+xsetwacom set "Tablet Monitor Pad pad" Button "16" "button 3"
+
+xsetwacom set "Tablet Monitor Pen stylus" Button "2" "key +b"
+xsetwacom set "Tablet Monitor Pen stylus" Button "3" "key +p"
